624cb552fdf6475f298d8a89a0fb01e9ce6bedec
Ayuba isiaka <105459982+ayubadevs@users.noreply.github.com>
2024-01-02 11:06:44 +0100

    main.js

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-let scores = 
-    wins:0,
-    loss:0,
-    ties:0
-    ;
+let scores = JSON.parse(localStorage.getItem('scores'))
 
 function playGame(playerMove) {
     const computerMove = pickComputerMove();
@@ -16,7 +12,7 @@ function playGame(playerMove) {
         }else if (computerMove === 'Scissors') {
             results = 'You win';
         }
-    }else if (playerMove === 'Paper') {
+     }else if (playerMove === 'Paper') {
         if (computerMove === 'Paper') {
             results = 'Tie';
         }else if (computerMove === 'Scissors') {
@@ -24,7 +20,7 @@ function playGame(playerMove) {
         }else if (computerMove === 'Rock') {
             results = 'You win';
         }
-    }else if (playerMove === 'Scissors') {
+     }else if (playerMove === 'Scissors') {
         if (computerMove === 'Scissors') {
             results = 'Tie';
         }else if (computerMove === 'Rock') {
@@ -32,52 +28,61 @@ function playGame(playerMove) {
         }else if (computerMove === 'Paper') {
             results = 'You win';
         }
-    }
+        };
 
         if (results === 'You win') {
             scores.wins ++;
         }else if (results === 'You loss') {
             scores.loss ++;
-        }else if ( results === 'Tie') {
+        }else if (results === 'Tie') {
             scores.ties ++;
         }
 
         localStorage.setItem('scores', JSON.stringify(scores));
 
-        document.querySelector('.moves').innerHTML = 
-
+    // CODE FOR DOM DISPLAY OF RESULTS,MOVES AND SCORES
+     document.querySelector('.move').innerHTML=
         `<p>
-        You
-        <img src="img/${playerMove}-emoji.png" class="last">
-        <img src="img/${computerMove}-emoji.png" class="last">
-        Computer
-    </p>`;
+            You
+            <img src="img/${playerMove}-emoji.png" class="one">
+            <img src="img/${computerMove}-emoji.png" class="one">
+            Computer
+        </p>`;
 
-        document.querySelector('.result').innerHTML = `Result: ${results}`;
-        
-        document.querySelector('.score').innerHTML = `Score's: Wins: ${scores.wins}, Loss: ${scores.loss}, Ties; ${scores.ties}`;
-   
-    };
+     document.querySelector('.result').innerHTML=
+     `Results: ${results}`;
+
+     document.querySelector('.score').innerHTML=
+     `Score's: Wins: ${ scores.wins},Loss: ${ scores.loss},Ties: ${ scores.ties}`;
+
+};
+
+
+
+function dosome() {
+    document.querySelector('.move').innerHTML=
+    `Pick a move to play !`;
+
+ document.querySelector('.result').innerHTML=
+ `Results: ?`;
+
+ document.querySelector('.score').innerHTML=
+ `Score's: Wins: 0,Loss: 0,Ties: 0`;
+
+ }
 
 
 function pickComputerMove() {
     const randomMove = Math.random();
     let computerMove = '';
-
     if (randomMove >= 0 && randomMove < 1 / 3) {
         computerMove = 'Rock';
-    }else if (randomMove >= 1 / 3 && randomMove <  2 / 3) {
+    }else if (randomMove >= 1 / 3 && randomMove < 2 / 3) {
         computerMove = 'Paper';
-    }else if ( randomMove >= 2 / 3 && randomMove < 1) {
+    }else if (randomMove >= 2 / 3 && randomMove < 1) {
         computerMove = 'Scissors';
     }
 
     return computerMove;
-};
-
-function overAll() {
-    
-    document.querySelector('.moves').innerHTML =`Pick a move to play`;
-    document.querySelector('.result').innerHTML = `Results: ?`;
-    document.querySelector('.score').innerHTML = `Score's: Wins: 0,Loss: 0,Ties: 0`;
+    // return randomMove;
 }
